1431e129f664492c2ce477dba2076c8d404aa873
David Clark <evil@digitalfivesyndey.com>
2012-07-27 15:16:02 +1000

    missed dirs

---
+++ HttpModule/ErrorGraphdat.cs
@@ -0,0 +1,31 @@
+﻿namespace Alphashack.Graphdat.Agent
+{
+    public class ErrorGraphdat : IGraphdat
+    {
+        private bool _usageErrorLogged;
+
+        public ErrorGraphdat()
+        {
+            HttpModule.SetupEventLog();
+        }
+
+        private void LogUsageError()
+        {
+            if (!_usageErrorLogged)
+            {
+                HttpModule.Logger(GraphdatLogType.ErrorMessage, null, "Graphdat API not found in HttpContext. HttpModule does not seem to be loaded.");
+                _usageErrorLogged = true;
+            }
+        }
+
+        public void Begin(string name)
+        {
+            LogUsageError();
+        }
+
+        public void End(string name)
+        {
+            LogUsageError();
+        }
+    }
+}
---
+++ Mvc3ApplicationTest/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using System.Web.Mvc;
+using Alphashack.Graphdat.Agent;
+
+namespace Mvc3ApplicationTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            var graphdat = HttpModule.SafeGetGraphdat();
+
+            graphdat.Begin("StepOne");
+            Thread.Sleep(200);
+            graphdat.End("StepOne");
+            graphdat.Begin("StepTwo");
+            graphdat.Begin("A");
+            Thread.Sleep(100);
+            graphdat.End("A");
+            graphdat.Begin("B");
+            Thread.Sleep(250);
+            graphdat.End("B");
+            graphdat.End("StepTwo");
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
